bd107fe82189c596964db42a0de3c7fd2d88f281
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-10-09 18:01:22 +0200

    First boards table.

---
+++ server/database/migrations/2019_10_09_155300_create_boards_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 127);
+            $table->string('code', 6)->unique();
+            $table->timestamps();
+            $table->index('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
