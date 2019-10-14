3b490a1e55a12270fb13220d5fdb77828dc49d55
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-10-14 11:04:36 +0200

    Sorting user results by name.

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return response()->json(Auth::user()->users()->get());
+        return response()->json(Auth::user()->users()->orderBy('name')->get());
     }
 
     /**
--- server/routes/web.php
+++ server/routes/web.php
@@ -15,7 +15,6 @@ $router->group(['prefix' => 'board'], function () use($router) {
     $router->get('{id}', ['uses' => 'BoardController@show']);
     $router->get('', ['middleware' => 'auth', 'uses' => 'BoardController@index']);
     $router->post('', ['uses' => 'BoardController@store']);
-    // $router->get('users', ['middleware' => 'auth', 'uses' => 'BoardController@users']);
 });
 
 $router->group(['prefix' => 'user', 'middleware' => 'auth'], function () use($router) {
