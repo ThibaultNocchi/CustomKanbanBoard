56c1b4a480c16dc7630142145eebd6efd463cd1a
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-10-18 00:23:00 +0200

    Server: added Cards table.

---
+++ server/database/migrations/2019_10_17_222156_create_cards_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('board_id');
+            $table->string('name', 127);
+            $table->unsignedInteger('order')->default(0);
+            $table->timestamps();
+            $table->unique(['board_id', 'name']);
+            $table->unique(['board_id', 'order']);
+            $table->foreign('board_id')->references('id')->on('boards')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
