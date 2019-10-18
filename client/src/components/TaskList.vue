557d08ec2975ecc01b60e984e536812e1b12f211
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-10-18 15:06:34 +0200

    Client: card dropdown.

--- client/src/components/TaskList.vue
+++ client/src/components/TaskList.vue
@@ -1,9 +1,39 @@
 <template>
-  
+
   <v-card>
-    <v-card-title>{{card.name}}</v-card-title>
+    <v-menu
+      open-on-click
+      close-on-content-click
+      bottom
+      offset-y
+      transition="slide-y-transition"
+    >
+      <template v-slot:activator="{on}">
+        <v-card-title>{{card.name}}
+          <v-spacer></v-spacer>
+          <v-btn
+            icon
+            v-on="on"
+          >
+            <v-icon>arrow_drop_down</v-icon>
+          </v-btn>
+        </v-card-title>
+      </template>
+
+      <v-list dense>
+        <v-list-item-group>
+          <v-list-item>
+            <v-list-item-icon class="mr-2"><v-icon dense color="error darken-1">delete</v-icon></v-list-item-icon>
+            <v-list-item-content class="error--text text--darken-1">
+              Delete this card
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+
+    </v-menu>
     <v-divider></v-divider>
-    
+
     <v-card-text>
 
       <!-- <task-card class="ma-1" v-for="(task, idx) in card.tasks" :key="idx" :task="task"></task-card> -->
@@ -15,10 +45,10 @@
 </template>
 
 <script>
-import TaskCard from '@/components/TaskCard.vue'
+import TaskCard from "@/components/TaskCard.vue";
 
 export default {
-  props: ['card'],
-  components: {TaskCard}
-}
+  props: ["card"],
+  components: { TaskCard }
+};
 </script>
