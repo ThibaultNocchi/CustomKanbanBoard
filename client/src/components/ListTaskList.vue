be5a2586aec76d2618cdb0295eb7ce7da21530f9
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-10-18 20:12:36 +0200

    Client: readded input.

--- client/src/components/ListTaskList.vue
+++ client/src/components/ListTaskList.vue
@@ -3,15 +3,34 @@
   <draggable
     v-model="cards_list"
     class="row"
+    draggable=".card-item"
   >
+
     <v-col
       cols="12"
       sm="6"
       v-for="card in cards_list"
       :key="card.name"
+      class="card-item"
     >
       <task-list :card="card"></task-list>
     </v-col>
+
+    <v-col
+      class="d-flex align-center justify-center"
+      cols="12"
+      sm="6"
+      slot="footer"
+    >
+      <new-button-input
+        txt-btn="New card"
+        txt-placeholder="Name"
+        txt-hint="New card name."
+        txt-error="Card already exists."
+        :send="submit_card"
+      ></new-button-input>
+    </v-col>
+
   </draggable>
 
 </template>
