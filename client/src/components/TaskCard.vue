15508fc81d9c470191700e91091a92c29f7c1d80
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-10-20 20:29:23 +0200

    Client: prepared changing description.

--- client/src/components/TaskCard.vue
+++ client/src/components/TaskCard.vue
@@ -4,15 +4,59 @@
     outlined
   >
     <v-card-text class="headline">{{task.name}}</v-card-text>
-    <v-card-text><span v-if="task.description">{{task.description}}</span><span
+    <v-card-text>
+
+      <div
+        v-if="!editing_desc"
+        @click="edit_desc"
+      >
+        <span v-if="task.description">{{task.description}}</span>
+        <span
+          v-else
+          class="font-italic"
+        >No description given.</span>
+      </div>
+
+      <v-form
+        onSubmit="return false"
         v-else
-        class="font-italic"
-      >No description given.</span></v-card-text>
+      >
+        <v-text-field
+          dense
+          ref="desc_input"
+          placeholder="New description"
+          v-model="task.description"
+          @blur="save_desc"
+        ></v-text-field>
+      </v-form>
+
+    </v-card-text>
   </v-card>
 </template>
 
 <script>
 export default {
-  props: ["task"]
+  props: ["task"],
+  data() {
+    return {
+      editing_desc: false
+    };
+  },
+  methods: {
+    edit_desc() {
+      this.editing_desc = true;
+      new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        });
+      }, 200).then(() => {
+        this.$refs.desc_input.focus();
+      });
+    },
+
+    save_desc() {
+      this.editing_desc = false
+    }
+  }
 };
 </script>
