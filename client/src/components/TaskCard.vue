08aef4270c69fc20809a9616d89599daebf309aa
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-10-20 18:52:48 +0200

    Client: added hint when no description available.

--- client/src/components/TaskCard.vue
+++ client/src/components/TaskCard.vue
@@ -1,12 +1,18 @@
 <template>
-  <v-card flat outlined>
-    <v-card-text class="subtitle-1">{{task.name}}</v-card-text>
-    <v-card-text>{{task.description}}</v-card-text>
+  <v-card
+    flat
+    outlined
+  >
+    <v-card-text class="headline">{{task.name}}</v-card-text>
+    <v-card-text><span v-if="task.description">{{task.description}}</span><span
+        v-else
+        class="font-italic"
+      >No description given.</span></v-card-text>
   </v-card>
 </template>
 
 <script>
 export default {
-  props: ['task']
-}
+  props: ["task"]
+};
 </script>
